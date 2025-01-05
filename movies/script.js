f068bb92fd11678bd6ca3e245436f851618cbeba
Harry Poter <yoyosamreen@gmail.com>
2025-01-05 13:43:24 +0530

    movies app by mahima

---
+++ movies/script.js
@@ -0,0 +1,16 @@
+// Example of data that might be fetched from an API or database
+const movieData = {
+  title: "The Great Adventure",
+  releaseDate: "February 25, 2025",
+  description:
+    "A thrilling tale of adventure and discovery, featuring an unlikely hero.",
+  posterUrl: "https://via.placeholder.com/300x450", // Placeholder image URL
+};
+
+// Assign the movie data to the HTML elements
+document.getElementById("movie-title").innerText = movieData.title;
+document.getElementById(
+  "release-date"
+).innerText = `Release Date: ${movieData.releaseDate}`;
+document.getElementById("description").innerText = movieData.description;
+document.getElementById("movie-pic").src = movieData.posterUrl;
